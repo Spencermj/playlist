e87b7b9ff201a3abb5cc56f5f7856f43057c3ea1
Matt Spencer <Spencermj@users.noreply.github.com>
2015-03-02 21:52:54 -0500

    added playlist code

--- playlist.py
+++ playlist.py
@@ -3,15 +3,24 @@ import pyechonest.playlist as playlist
 import sys
 import os
 
-def main():
+def main(radio_type, paramaterList):
     collect = []
-    p = playlist.static(type='artist-radio', artist=['led zeppelin', 'rolling stones'])
+    if radio_type == "artist-radio":
+        p = playlist.static(type=radio_type, artist=paramaterList)
+    elif radio_type == "song-radio":
+        p = playlist.static(type=radio_type, song=paramaterList)
+    elif radio_type == "genre-radio":
+        p = playlist.static(type=radio_type, genre=paramaterList)
+    p = playlist.static(type="artist", artist=paramaterList)
     for song in p:
         print song.title
 
 if __name__ == '__main__':
     try:
-        main()
+        radioType = sys.argv[1]
+        criteriaList = []
+        for i in range(len(sys.argv) - 2):
+             criteriaList.append(sys.argv[i + 2])
     except:
         sys.exit(-1)
-
+    main(radioType, criteriaList)
